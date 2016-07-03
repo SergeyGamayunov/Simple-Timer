d7450d8f2ae652c4ccd22f1123030d15910e66ab
Сергей Гамаюнов <Sergey@imac-sergej.dlink>
2016-07-03 17:22:36 +0300

    Initial Commit

---
+++ SimpleTImer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTImer
+//
+//  Created by Сергей Гамаюнов on 03.07.16.
+//  Copyright © 2016 Сергей Гамаюнов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
